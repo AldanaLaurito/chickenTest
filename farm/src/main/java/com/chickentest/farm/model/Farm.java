7534a7b46187168d33d66516306b61ab86fdfe6d
Aldana Laurito <aldana.laurito.AL@gmail.com>
2018-08-14 15:51:47 -0300

    Classes
    Al microservices connect correctly to the server

--- farm/src/main/java/com/chickentest/farm/model/Farm.java
+++ farm/src/main/java/com/chickentest/farm/model/Farm.java
@@ -1,4 +1,37 @@
 package com.chickentest.farm.model;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
 public class Farm {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @OneToMany(mappedBy="farm", fetch=FetchType.EAGER, cascade = CascadeType.ALL)
+    private Set<Chicken> chickens;
+
+    public Farm(){}
+
+    public Farm(Long id, Set<Chicken> chickens) {
+        this.id = id;
+        this.chickens=chickens;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Set<Chicken> getChickens() {
+        return chickens;
+    }
+
+    public void setChickens(Set<Chicken> chickens) {
+        this.chickens = chickens;
+    }
 }
